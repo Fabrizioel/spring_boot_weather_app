9939775a219f2723394a41ec912d8ce874a594c4
Fabrizioel <fabrizio@itic.com.pe>
2023-08-10 18:03:49 -0500

    constants added to eliminate magic strings

--- src/main/java/com/example/demo/Constants/Constants.java
+++ src/main/java/com/example/demo/Constants/Constants.java
@@ -1,6 +1,10 @@
 package com.example.demo.Constants;
 
 public class Constants {
+    /* Yes or No for API call */
+    public static final String YES = "yes";
+    public static final String NO = "no";
+
     /* Air Quality */
     public static final String GOOD_AQ = "Good";
     public static final String MODERATE_AQ = "Moderate";
@@ -9,7 +13,11 @@ public class Constants {
     public static final String VERY_UNHEALTHY_AQ = "Very unhealthy";
     public static final String HAZARDOUS_AQ = "Hazardous";
 
-    /* Yes / No for API call */
-    public static final String YES = "yes";
-    public static final String NO = "no";
+    /* Air Quality Colors */
+    public static final String GOOD_AQ_COLOR = "#40bf32";
+    public static final String MODERATE_AQ_COLOR = "#8ebf32";
+    public static final String UNHEALTHY_SG_AQ_COLOR = "#9ebf32";
+    public static final String UNHEALTHY_AQ_COLOR = "#bfba32";
+    public static final String VERY_UNHEALTHY_AQ_COLOR = "#bf8732";
+    public static final String HAZARDOUS_AQ_COLOR = "#bf4c32";
 }
